ea9ff75b1da40c68ef1c1d4f3dea63ec62333e3c
SimonV <SimonV6@gmail.com>
2018-04-29 10:00:13 +0300

    rewrote summary controller as async/await

--- controllers/summary_controller.js
+++ controllers/summary_controller.js
@@ -2,59 +2,51 @@ var Store = require('../models/store_model')
 var Product = require('../models/product_model')
 
 const summariser = (accumulator, currentValue) => {
-    var store = accumulator[currentValue["store_id"]];
-
-    store = store || { count: 0, min_price: Number.MAX_SAFE_INTEGER, max_price: Number.MIN_SAFE_INTEGER, avg_price: 0 };
-    
-    if (store['min_price'] > currentValue['price']){
-        store['min_price'] = currentValue['price'];
+     
+    if (accumulator['min_price'] > currentValue['price']){
+        accumulator['min_price'] = currentValue['price'];
     }
 
-    if (store['max_price'] < currentValue['price']){
-        store['max_price'] = currentValue['price'];
+    if (accumulator['max_price'] < currentValue['price']){
+        accumulator['max_price'] = currentValue['price'];
     }
 
-    var old_total_price = store['avg_price'] * store['count'];
-    store['avg_price'] = (old_total_price + currentValue['price']) / (store['count'] + 1);
+    var old_total_price = accumulator['avg_price'] * accumulator['count'];
+    accumulator['avg_price'] = (old_total_price + currentValue['price']) / (accumulator['count'] + 1);
 
-    store['count']++;
+    accumulator['count']++;
 
-    accumulator[currentValue["store_id"]] = store;
     return accumulator;
 };
 
-const get_store_name = async (store_id) => {
-    return Store.findById(store_id)
-    .then(store => {
-        return store['name'];
-    })
-    .catch(err => {
-        console.log(err);
-        throw err;
-    });
+
+const store_summariser = async (currentValue) => {
+    
+    var products =  await Product.find({store_id: currentValue['_id']});
+    var store_summary = products.reduce(summariser, { 
+            name: currentValue['name'],
+            count: 0, 
+            min_price: Number.MAX_SAFE_INTEGER, 
+            max_price: Number.MIN_SAFE_INTEGER, 
+            avg_price: 0 
+        });
+
+    if (store_summary['count'] === 0){
+        store_summary['min_price'] = 0;
+        store_summary['max_price'] = 0;
+    }
+    return store_summary;   
 }
 
 var controller = {
-    generateSummary: function(){
+    generateSummary: async function(){
         console.log('Generating summary');
 
         var store_summary = {}; 
 
-        return Product.find({}).then( products => {
-            return products.reduce(summariser,{});
-        }).then( summary => {
-            return Promise.all(Object.keys(summary).map(store_id => {
-                return get_store_name(store_id).then(store_name => {
-                    store_summary[store_name] = summary[store_id];
-                });
-            }));
-        }).then(() => {
-            console.log(store_summary);
-            return store_summary;
-        }).catch(err => {
-            console.log("Error while generating summary", err);
-            return {};
-        });
+        var stores = await Store.find({});
+        var store_summaries = await Promise.all(stores.map(store_summariser));
+        return store_summaries;
     }
 }
 
