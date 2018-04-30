affd5a10e9d262352aed027f3579b20a3f807dc9
SimonV <SimonV6@gmail.com>
2018-04-30 16:46:35 +0300

    implemented product controller

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -4,53 +4,73 @@ var Product = require('../models/product_model')
 const SORT_DESC = -1;
 const SORT_ASC = 1;
 
-const validate_limit = (limit) => {
-    return Number.isInteger(limit) && limit > 0;
+const build_filter_clause = (filter) => {
+    var result = true;
+    Object.keys(filter).forEach(key => {
+        if (!Product.schema.obj.hasOwnProperty(key)) {
+            throw new TypeError('Unsupported filter field: ' + key);
+        }
+        if (typeof(filter[key]) != Product.schema.obj[key].name.toLowerCase()){
+            throw new TypeError('Invalid filter field value type for field: ' + key);
+        }
+    })
+    return filter;
+}
+
+const build_sort_clause = (sort) => {
+    if (Object.keys(sort).length != 1){
+        throw new TypeError('Only sorting by a single field is supported');
+    }
+    var key = Object.keys(sort)[0];
+
+    if (!Product.schema.obj.hasOwnProperty(key)){
+        throw new TypeError('Invalid sort field name: '+ key);
+    }
+
+    if (sort[key].toLowerCase() == "asc"){
+        sort[key] = SORT_ASC;
+    }else if (sort[key].toLowerCase() == "desc"){
+        sort[key] = SORT_DESC;
+    }else{
+        throw new TypeError('Unsupported sort field value: '+ sort[key]);
+    }
+    return sort;
+}
+
+const build_limit = (limit) => {
+    if (!Number.isInteger(limit)){
+        throw new TypeError('Unsupported limit field value: '+ limit);
+    }
+    if (limit < 1){
+        throw new TypeError('Limit field value can not be negative');
+    }
+    return limit;
 }
 
 var controller = {
-
     findProducts: async function(parameters){
-        //console.log('Looking for products matching:' , parameters);
-
         var query = Product;
-        var find_clause = {};
-        var sort_clause = {};
-        var limit_clause = null;
         
+        var filter_clause = {};
+        var sort_clause = {};
+
         //Validate parameter values
-        query = query.find({});
-        // if (parameters.hasOwnProperty('filter')){
-        //     if (validate_filter(parameters.filter)){
-        //         find_clause = build_find_clause(parameters.filter);
-        //     }else{
-        //         throw new TypeError('Failed to parse filter field');
-        //     }
-        // }
-
-        query = query.sort({'price': SORT_ASC});
-        // if (parameters.hasOwnProperty('sort')){
-        //     if (validate_sort(parameters.sort)){
-        //         sort_clause = build_find_clause(parameters.sort);
-        //     }else{
-        //         throw new TypeError('Failed to parse sort field');
-        //     }
-        // }
-
-        query = query.limit(10);
-        if (parameters.hasOwnProperty('limit')){
-            if (validate_limit(parameters.limit)){
-                query = query.limit(parameters.limit);
-            }else{
-                throw new TypeError('Invalid limit field value');
-            }
+        if (parameters.hasOwnProperty('filter')){
+            query = query.find(build_filter_clause(parameters.filter));
+        }else{
+            query = query.find({});
         }
 
-        //Build Query
-        //var products = await Product.find(find_clause).sort({'price': SORT_ASC}).limit(10);
+        if (parameters.hasOwnProperty('sort')){
+            query = query.sort(build_sort_clause(parameters.sort));
+        }
+
+        if (parameters.hasOwnProperty('limit')){
+            query = query.limit(build_limit(parameters.limit));
+        }
+
         var products = await query.exec();
         return products;
-
     }
 }
 
